9be4864521a5139fcd86f7be105927cef145335d
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-16 22:23:55 +0000

    first java lessons

--- lesson7.java
+++ lesson7.java
@@ -19,7 +19,7 @@ $ javac Compiling.java
                                   (list the files in the directory - would be lesson7.java in this case - class would be the same)
 $ ls
 Compiling.class  Compiling.java
-                                   (run the compiled file - would be lesson7 in this case)
+                                   (run the compiled file - would be lesson7 in this case - class still the same)
 $ java Compiling
 Java is a class-based language.
 Java classes have a 'main' method.
