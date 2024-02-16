26fc2e3e73f4efd0a9146c70300a15619ce302a1
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-16 21:53:46 +0000

    first java lessons

---
+++ lesson3.java
@@ -0,0 +1,9 @@
+public class HideAndSeek {
+    public static void main(String[] args) {
+      System.out.println("Let's play hide and seek.");
+      System.out.print("three...");
+      System.out.print("two...");
+      System.out.println("one...");
+      System.out.println("Ready or not, here I come!");
+    }
+  }
