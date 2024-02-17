df885b97e9c5f2895d1c5a694687c035a5683709
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-17 18:09:54 +0000

    further lessons

---
+++ lesson17.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+String name = "Richard";
+int age = 34;
+double desiredSalary = 45000.00;
+char gender = 'm';
+boolean lookingForJob = true;
+	}
+}
